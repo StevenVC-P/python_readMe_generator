9ec9f1b006bcf315012684c77e5f912459edde34
StevenVC-P <77998885+StevenVC-P@users.noreply.github.com>
2021-07-28 16:10:25 -0500

    Set up initial prompt questions for the console

---
+++ dict.py
@@ -0,0 +1,15 @@
+months = {}
+months['January'] = 'jan'
+months['February'] = 'feb'
+months['March'] = 'mar'
+months['April'] = 'apr'
+months['May'] = 'may'
+months['June'] = 'jun'
+months['July'] = 'jul'
+months['August'] = 'aug'
+months['September'] = 'sep'
+months['October'] = 'oct'
+months['November'] = 'nov'
+months['December'] = 'dec'
+
+month = selectFromDict(months, 'Month')
